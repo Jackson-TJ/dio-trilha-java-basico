1e0e7782acca2883e689102de75b5ca3309f361a
JACKSON RODRIGUES DA SILVA <126480307+Jackson-TJ@users.noreply.github.com>
2023-03-10 16:13:40 -0300

    Add files via upload

---
+++ tipovariaveis.java
@@ -0,0 +1,15 @@
+public class tipovariaveis {
+    public static void main (String[] args)  {
+        
+        
+        int modulo = 20 % 3;
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+
+
+        System.out.println(modulo);
+
+    }
+}
