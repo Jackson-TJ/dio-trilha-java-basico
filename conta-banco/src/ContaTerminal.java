303dd7727dd5c4e33bb3861d845e5c4afa697ac2
JacksonTJ <jacksoncoteminas@gmail.com>
2023-09-12 15:52:05 -0300

    Novo projeto de condicional

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,20 +2,30 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
-        Scanner auxiliar = new Scanner(System.in);
+            Scanner nome = new Scanner(System.in);
+            Scanner auxiliar = new Scanner(System.in);
+            String agenciaNumero;
+            int numeroConta;
+            String nomeCliente;
+            Double saldo;
+                  
+            System.out.println("Por favor, digite o número da Agência !");
+                agenciaNumero = auxiliar.nextLine();
 
-        String agenciaNumero;
-        int numeroConta;
-        String nomeCliente;
-        Double saldo;
-              
-        System.out.println("Digite o numero da sua Agências :");
-            agenciaNumero = auxiliar.nextLine()
-        System.out.println("Digite o numero da Conta: ");
-            numeroConta = auxiliar.nextInt();
+            System.out.println("Por favor, digite o número da Conta !");
+                numeroConta = auxiliar.nextInt();
                 
-    
-    
+            System.out.println("Por favor, digite o seu nome !");            
+                nomeCliente = nome.nextLine();
+
+            System.out.println("Por favor, digite o Saldo !");
+                saldo = auxiliar.nextDouble();
+            System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é  " +agenciaNumero+",");
+            System.out.println(" conta  "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque");
+            nome.close();
+        auxiliar.close();
     }
+
+    
+    
 }
